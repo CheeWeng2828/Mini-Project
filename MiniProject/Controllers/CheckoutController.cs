f38f4512c5c5905aa62f91c2b0d787c9d7c1e387
CheeWeng2828 <lamcw-wm24@student.tarc.edu.my>
2025-12-18 10:10:07 +0800

    Status Redesign

--- MiniProject/Controllers/CheckoutController.cs
+++ MiniProject/Controllers/CheckoutController.cs
@@ -5,7 +5,7 @@ using System.Net.Mail;
 using System.Text;
 using System.Text.Json.Nodes;
 
-namespace Assignment.Controllers
+namespace MiniProject.Controllers
 {
     public class CheckoutController : Controller
     {
